d180a8436f8d5db305c9ae69a146747333058b22
Fábio Luiz <142447343+Fabio42-sys@users.noreply.github.com>
2024-02-05 03:41:49 +0000

    feat: criando primeiro código ts

---
+++ meuCodigo.js
@@ -0,0 +1,6 @@
+// meuCodigo.ts
+function dizerOla(nome) {
+    console.log("Ol\u00E1, ".concat(nome, "!"));
+}
+var meuNome = "Usuário";
+dizerOla(meuNome);
---
+++ meuCodigo.ts
@@ -0,0 +1,35 @@
+function dizerOla(nome: string): void {
+  console.log(`Olá, ${nome}!`);
+}
+
+  const meuNome: string = "Usuário";
+  dizerOla(meuNome);
+
+
+function perguntarTudoBem(bem: string): void {
+  console.log(`Tudo ${bem}?`);
+}
+
+const tudoBem: string = "Pergunta";
+perguntarTudoBem(tudoBem);
+
+function bomDia(dia: string): void {
+  console.log(`bom ${dia}!`);
+}
+
+const dia: string = "recall";
+bomDia(dia);
+
+function boaTarde(tarde: string): void {
+  console.log(`boa ${tarde}!`);
+}
+
+const tarde: string = "recall";
+boaTarde(tarde);
+
+function boaNoite(noite: string): void {
+  console.log(`boa ${noite}`);
+}
+
+const noite: string = "recall";
+boaNoite(noite);
